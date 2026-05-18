557da0e4c7f2810d23a7a22b657bb05dc1660626
potterliu0708-hub <potterliu0708@gmail.com>
2026-05-18 00:02:39 -0500

    chore: 優化 ESLint 配置，新增 react-hooks 規則

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,42 @@
 module.exports = {
   root: true,
-  env: { 
-    browser: true, 
-    es2021: true, 
-    node: true 
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
   },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-native/all',
-    'prettier'
+    'prettier',
   ],
-  parserOptions: { 
-    ecmaVersion: 'latest', 
-    sourceType: 'module' 
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
-  plugins: ['react', 'react-native'],
+  plugins: ['react', 'react-native', 'react-hooks'],
   rules: {
-    'react/prop-types': 'off',
+    'react/prop-types': 'off',                    // 暫時關閉（未來可改用 TypeScript）
+    'react/react-in-jsx-scope': 'off',
     'react-native/no-inline-styles': 'warn',
     'no-console': ['warn', { allow: ['warn', 'error'] }],
-    'no-unused-vars': 'warn',
-    'react/react-in-jsx-scope': 'off'   // React 17+ 已不需要 import React
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    
+    // React Hooks 規則（非常重要）
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+
+    // 其他建議
+    'prefer-const': 'warn',
+    'no-var': 'error',
   },
-  settings: { 
-    react: { version: 'detect' } 
-  }
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
